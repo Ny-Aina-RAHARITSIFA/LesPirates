94cd88f1227f9ceb11fc4cc9aa465e75817fce6c
Ny Aina <nyainamiradoraharitsifa1707@gmail.com>
2026-02-12 09:48:48 +0100

    Début de la création du jeu

---
+++ src/jeu/Jeu.java
@@ -0,0 +1,23 @@
+package jeu;
+
+public class Jeu {
+	private int nbJoueurs;
+	private Pion[] pions = new Pion[5];
+	private Joueur[] joueurs = new Joueur[2];
+	
+	public void demarrerJeu() {
+		
+	}
+	
+	public void creerJoueur(String nom,Pirate pirate) {
+		
+	}
+	
+	public int donnerCaseActuelle(Joueur joueur) {
+		
+	}
+	
+	public boolean estFini() {
+		
+	}
+}
